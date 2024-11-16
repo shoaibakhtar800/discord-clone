aad1a9ba68384fb2d838d21106e401542234bfe5
shoaibakhtar800 <cs08018akp@gmail.com>
2024-11-16 15:31:28 +0530

    implementing navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,8 +1,11 @@
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-import { currentProfile } from "@/lib/current-profile"
-import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { currentProfile } from "@/lib/current-profile";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "@/components/navigation/navigation-item";
+import { NavigationAction } from "@/components/navigation/navigation-action";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -26,6 +29,20 @@ export const NavigationSidebar = async () => {
             className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
         >
             <NavigationAction />
+            <Separator
+                className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
         </div>
     )
 }
