607ba84ad2ed62f540b337fc3d0107052b8d7204
shoaibakhtar800 <cs08018akp@gmail.com>
2024-11-23 22:54:17 +0530

    adding invitation

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { ChannelType } from "@prisma/client";
 
 import { db } from "@/lib/db";
+import { ServerHeader } from "./server-header";
 import { currentProfile } from "@/lib/current-profile";
 
 interface ServerSidebarProps {
@@ -38,11 +39,23 @@ export const ServerSidebar = async ({
         }
     });
 
+    if (!server) {
+        return redirect("/");
+    }
+
     const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+    const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+    const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
+    const members = server?.members.filter((member) => member.profileId !== profile.id);
+
+    const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
     return (
-        <div>
-            Server Sidebar
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+            <ServerHeader 
+                server={server}
+                role={role}
+            />
         </div>
     )
 }
