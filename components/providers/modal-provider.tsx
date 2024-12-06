620f0d325d282ef950f3cb7056dbbf63a0cf99de
shoaibakhtar800 <cs08018akp@gmail.com>
2024-12-06 23:20:07 +0530

    minor change

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
-import { CreateServerModal } from "@/components/modals/create-server-modal";
-import { CreateChannelModel } from "@/components/modals/create-channel-modal";
+import { EditChannelModel } from "@/components/modals/edit-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { CreateChannelModel } from "@/components/modals/create-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -29,6 +31,8 @@ export const ModalProvider = () => {
             <CreateChannelModel />
             <LeaveServerModal />
             <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModel />
         </>
     )
 }
