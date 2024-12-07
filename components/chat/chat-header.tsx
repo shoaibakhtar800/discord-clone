d7723f463c169f1e33b841a47badf3bad35c0096
shoaibakhtar800 <cs08018akp@gmail.com>
2024-12-07 15:58:34 +0530

    socket io

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 
+import { UserAvatar } from "@/components/user-avatar";
 import { MobileToggle } from "@/components/mobile-toggle";
 
 interface ChatHeaderProps {
@@ -22,6 +23,12 @@ const ChatHeader = ({
             />
             {type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+            )}
+            {type === "conversation" && (
+                <UserAvatar 
+                    src={imageUrl}
+                    className="h-8 w-8 md:h-8 md:w-8 mr-2"
+                />
             )}
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
